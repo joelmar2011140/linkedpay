2caf16b5820e49e0c298762bb25ea391afa9a5f7
Joel Marinho <joeldeveloper2011140@gmail.com>
2023-03-10 20:52:46 +0100

    fix: remove kw and description props in StandardLayout Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import StandardLayout from "@/layouts/StandardLayout"
 
 export default function HomePage() {
   return (
-    <StandardLayout titulo="Finança digital que adequa-se ao seu bolso" kw="" descricao="">
+    <StandardLayout titulo="Finança digital que adequa-se ao seu bolso">
       <h1>Olá mundo</h1>
     </StandardLayout>
   )
